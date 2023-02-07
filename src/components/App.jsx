cd1b9fda4657d29648deb9007f4f2b553aecfa7e
Nataliia Shkarupeta <natalinardi.kh@gmail.com>
2023-02-07 15:38:20 +0200

    work

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,26 @@
+import { useState } from 'react';
+import { Button } from './Button/Button';
+import { useDispatch } from 'react-redux';
+import { fetchUsers } from 'redux/users/usersOperations';
+import { useSelector } from 'react-redux';
+import { selectUsers } from 'redux/users/usersSelector';
+import { UsersList } from './UsersList/UsersList';
+
 export const App = () => {
+  const [ setIsListShow] = useState(false);
+  const dispatch = useDispatch();
+  const users = useSelector(selectUsers);
+  console.log(users);
+
+  const showUsersList = () => {
+    setIsListShow(true);
+    dispatch(fetchUsers());
+  };
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <>
+      <Button text="Show list of users" clickHandler={showUsersList} />
+      <UsersList users={users} />
+    </>
   );
 };
